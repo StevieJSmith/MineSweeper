9bf74dd26260945f63371e59ac42e5155af18c7e
Steven Smith <smithjstevie@gmail.com>
2024-02-29 15:53:44 +0000

    Calculating bomb positions function added

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -16,13 +16,14 @@ public class Game implements ActionListener {
 	private JPanel buttonPanel;
 	private JButton[][] buttons; // Multidimensional Array of buttons
 	private JLabel textField;
+	int[][] numOfMines; // store the number of mines around a position
 
 	public Game() {
 
 		random = new Random();
 		int count = 0;
-		boardSize = 12;
-		bombAmount = 20;
+		boardSize = 6;
+		bombAmount = 10;
 		bombCoordinates = new ArrayList<>();
 
 		// bomb position generation (loop till the bomb amount is reached)
@@ -63,7 +64,9 @@ public class Game implements ActionListener {
 		textField.setForeground(Color.GREEN);
 		textField.setText("Watch out sweeper, there are " + bombAmount + " Bombs");
 
+		numOfMines = new int[boardSize][boardSize];
 		buttons = new JButton[boardSize][boardSize];
+
 		for(int i = 0; i < buttons.length; i++){
 			for(int j = 0; j < buttons.length; j++){
 				buttons[i][j] = new JButton(); // current button
@@ -82,8 +85,57 @@ public class Game implements ActionListener {
 		gameFrame.setSize(600,600);
 		gameFrame.revalidate();
 		gameFrame.setLocationRelativeTo(null); // centers the frame in the middle of the screen
+		gameFrame.setVisible(true);
+
+		getMines();
+
+
 
 	}
+
+
+	public void getMines() {
+		for(int i = 0; i < numOfMines.length; i++){ // current coordinates (x,y)
+			for(int j = 0; j < numOfMines.length; j++){
+
+				boolean changed = false;
+				int mineTotal = 0;
+
+				for(int q = 0; q < bombCoordinates.size(); q++) { // loop through all mine positions
+
+					if(j == bombCoordinates.get(q).x && i == bombCoordinates.get(q).y) { //
+
+						numOfMines[i][j] = boardSize+1; // set mines to be 1 larger than the board size
+						changed = true;
+					}
+				}
+
+				if(!changed) { // if it was not a mine, calculate how many mines are around
+
+					for(int g = 0; g < bombCoordinates.size(); g++) { // positions around the location
+						if((j+1 == bombCoordinates.get(g).x && i+1 == bombCoordinates.get(g).y) // SE
+						|| (j-1 == bombCoordinates.get(g).x && i-1 == bombCoordinates.get(g).y) // NW
+						|| (j+1 == bombCoordinates.get(g).x && i == bombCoordinates.get(g).y) // 
+						|| (j == bombCoordinates.get(g).x && i+1 == bombCoordinates.get(g).y)
+						|| (j-1 == bombCoordinates.get(g).x && i == bombCoordinates.get(g).y)
+						|| (j == bombCoordinates.get(g).x && i-1 == bombCoordinates.get(g).y)
+						|| (j+1 == bombCoordinates.get(g).x && i-1 == bombCoordinates.get(g).y)
+						|| (j-1 == bombCoordinates.get(g).x && i+1 == bombCoordinates.get(g).y)) {
+							mineTotal++;
+						}
+						numOfMines[i][j] = mineTotal;
+					}
+				}
+			}
+		}
+		for(int a = 0; a < numOfMines.length; a++) {
+			for(int b = 0; b < numOfMines.length; b++) {
+				System.out.print(numOfMines[a][b]+" ");
+			}
+			System.out.println();
+		}
+	}
+
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
